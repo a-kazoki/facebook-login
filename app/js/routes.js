8c094973805087f6dd8b1f155413ed6077d3d2ba
a-kazoki <ahmedammarsalah@gmail.com>
2017-01-19 15:34:02 -0600

    adjust5

--- app/js/routes.js
+++ app/js/routes.js
@@ -1,18 +1,18 @@
 myApp.config(["$routeProvider", function($routeProvider) {
     $routeProvider
-    .when("/", {
-        templateUrl : "views/home/login.html",
-        controller : "homeCtrl"
-    })
-    .when("/", {
-        templateUrl : "views/home/dashboard.html",
-        controller : "dashboardCtrl",
-        authenticated : true
-    })
-    .otherwise("/", {
-        templateUrl : "views/home/home.html",
-        controller : "homeCtrl"
-    })
+        .when("/", {
+            templateUrl : "views/home/login.html",
+            controller : "homeCtrl"
+        })
+        .when("/", {
+            templateUrl : "views/home/dashboard.html",
+            controller : "dashboardCtrl",
+            authenticated : true
+        })
+        .otherwise("/", {
+            templateUrl : "views/home/home.html",
+            controller : "homeCtrl"
+        });
 }]);
 
 myApp.run(["$routeScope", "$location", "authFact", function ($routeScope, $location, authFact) {
