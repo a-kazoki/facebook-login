0ec890669cbed39fbd9c212f49b6212e7e7ac5b0
a-kazoki <ahmedammarsalah@gmail.com>
2017-01-19 14:38:05 -0600

    log responce

--- app/js/homeCtrl.js
+++ app/js/homeCtrl.js
@@ -6,6 +6,7 @@ myApp.controller("homeCtrl", ["$scope", function ($scope) {
                 console.log('Welcome!  Fetching your information.... ');
                 FB.api('/me', function (response) {
                     console.log('Good to see you, ' + response.name + '.');
+                    console.log(response);
                 });
             } else {
                 console.log('User cancelled login or did not fully authorize.');
