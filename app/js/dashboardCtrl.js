cc233aa3160cabdf5ab9f33c60d261bed3f6f7c2
a-kazoki <ahmedammarsalah@gmail.com>
2017-01-26 14:19:56 -0600

    adjust 52

--- app/js/dashboardCtrl.js
+++ app/js/dashboardCtrl.js
@@ -3,12 +3,14 @@ myApp.controller("dashboardCtrl", ["$scope", "$location", "$cookies", function (
     $scope.theid = favoriteCookie;
     var allcookies = $cookies.getAll();
     console.log(favoriteCookie);
+    console.log(theid);
     console.log(allcookies);
     
     $scope.logout = function () {
         $location.path("/");
         $cookies.remove("accessToken");
         $cookies.remove("userid");
-        $scope.theid = "you are not loged in";
+        $scope.theid = "";
+        console.log(theid);
     };
 }]);
