25f8ae88e3f97781f6abfc89dbe3e98de0303bd4
a-kazoki <ahmedammarsalah@gmail.com>
2017-01-26 12:25:07 -0600

    adjust 33

--- app/js/dashboardCtrl.js
+++ app/js/dashboardCtrl.js
@@ -1,4 +1,4 @@
 myApp.controller("dashboardCtrl", ["$scope", "$cookies", function ($scope, $cookies) {
-    //var userObj = $cookies.get("userObj");
-    console.log(userObj);
+    var userObj = $cookies.get("userObj");
+    console.log(userObj.name);
 }]);
