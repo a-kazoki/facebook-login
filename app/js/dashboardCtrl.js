4b5357f239fbe18ccfda390544e46fc2e764b1d5
a-kazoki <ahmedammarsalah@gmail.com>
2017-01-26 13:47:59 -0600

    adjust 47

--- app/js/dashboardCtrl.js
+++ app/js/dashboardCtrl.js
@@ -1,5 +1,6 @@
 myApp.controller("dashboardCtrl", ["$scope", "$cookies", function ($scope, $cookies) {
     var favoriteCookie = $cookies.get('userid');
+    $scope.theid = favoriteCookie;
     var allcookies = $cookies.getAll();
     console.log(favoriteCookie);
     console.log(allcookies);
