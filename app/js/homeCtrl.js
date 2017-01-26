362ad11b4bd1945745b63821fe7213d28ef3c9c7
a-kazoki <ahmedammarsalah@gmail.com>
2017-01-26 17:10:14 -0600

    adjust 58

--- app/js/homeCtrl.js
+++ app/js/homeCtrl.js
@@ -1,22 +1,26 @@
 myApp.controller("homeCtrl", ["$scope", "authFact", "$location", "$cookies", function ($scope, authFact, $location, $cookies) {
     $scope.name = "Login Please";
     $scope.FBLogin = function () {
-        FB.login(function (response) {
-            if (response.authResponse) {
-                console.log('Welcome!  Fetching your information.... ');
-                FB.api('/me', function (response) {
-                    console.log('Good to see you, ' + response.name + '.');
-                    console.log(response);
-                    $cookies.put('userid', response.id);
-                    var accessToken = FB.getAuthResponse().accessToken;
-                    console.log(accessToken);
-                    authFact.setAccessToken(accessToken);
-                    $location.path("/dashboard");
-                    $scope.$apply();
-                });
-            } else {
-                console.log('User cancelled login or did not fully authorize.');
-            }
-        }, {scope: 'email'});
+//        FB.login(function (response) {
+//            if (response.authResponse) {
+//                console.log('Welcome!  Fetching your information.... ');
+//                FB.api('/me', function (response) {
+//                    console.log('Good to see you, ' + response.name + '.');
+//                    console.log(response);
+//                    $cookies.put('userid', response.id);
+//                    var accessToken = FB.getAuthResponse().accessToken;
+//                    console.log(accessToken);
+//                    authFact.setAccessToken(accessToken);
+//                    $location.path("/dashboard");
+//                    $scope.$apply();
+//                });
+//            } else {
+//                console.log('User cancelled login or did not fully authorize.');
+//            }
+//        }, {scope: 'email'});
+        FB.login(function(response) {
+            console.log(response);
+  // handle the response
+}, {scope: 'email'});
     };
 }]);
