f1d17e901b514ea60898c921233e7b4346f5e3c3
a-kazoki <ahmedammarsalah@gmail.com>
2017-01-28 22:04:27 -0600

    adjust 65

--- app/js/homeCtrl.js
+++ app/js/homeCtrl.js
@@ -4,7 +4,7 @@ myApp.controller("homeCtrl", ["$scope", "authFact", "$location", "$cookies", fun
         FB.login(function (response) {
             if (response.authResponse) {
                 console.log('Welcome!  Fetching your information.... ');
-                FB.api('/me' , {fields: 'id,name,email,public_profile'}, function (response) {
+                FB.api('/me' , {fields: 'id,name,email,picture'}, function (response) {
                     console.log('Good to see you, ' + response.name + '.');
                     console.log(response);
                     $cookies.put('userid', response.id);
